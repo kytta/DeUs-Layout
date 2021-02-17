53eaf6841973204ab50f324c7f01b15b4a3b14b2
Nikita Karamov <nick@karamoff.dev>
2021-02-17 14:02:48 +0100

    Update build, package and install scripts

--- build-icons.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iconutil --convert icns --output ./src/mac/Migrant.bundle/Contents/Resources/English\ \(DE\).icns ./src/mac/English.iconset
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-touch ./src/mac/Migrant.bundle
-
-sudo rm -rf /Library/Keyboard\ Layouts/Migrant.bundle
-sudo cp -r ./src/mac/Migrant.bundle /Library/Keyboard\ Layouts/
